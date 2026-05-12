c76d1eb4d32b5261b9b661547835645537f95fcb
engeneerdDavid <broudavid505@gmail.com>
2026-05-12 23:01:28 +0000

    brand: switch to google satellite hybrid view for maximum terrain clarity

--- client/src/pages/PublicMap.jsx
+++ client/src/pages/PublicMap.jsx
@@ -201,8 +201,8 @@ const PublicMap = () => {
                 <MapBoundsSetter reports={reports} />
                 <MapAutoFixer trigger={activeFilter} />
                 <TileLayer 
-                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" 
-                    attribution='&copy; CARTO' 
+                    url="https://mt1.google.com/vt/lyrs=y,h&x={x}&y={y}&z={z}" 
+                    attribution='&copy; Google' 
                     noWrap={true}
                 />
                 
